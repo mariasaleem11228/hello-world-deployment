7b1333967ca331b096588c41da59d9e6609b5620
masal006 <maria.saleem006@stud.fh-dortmund.de>
2025-06-19 13:56:01 +0200

    test fail check

--- Backend.Test/UnitTest1.cs
+++ Backend.Test/UnitTest1.cs
@@ -7,7 +7,7 @@ public class UnitTest1
     [Fact]
     public void HelloWorld_ReturnsExpectedMessage()
     {
-        string result = "Hello World backend .Net - PR merged!";
+        string result = "Hello World backend .Net - merged!";
         Assert.Equal("Hello World backend .Net - PR merged!", result);
     }
 }
