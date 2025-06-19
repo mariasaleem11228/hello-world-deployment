137932043cae790c75bb9f6c45889dbb1acaa6d7
masal006 <maria.saleem006@stud.fh-dortmund.de>
2025-06-19 13:51:17 +0200

    fix

--- Backend.Test/UnitTest1.cs
+++ Backend.Test/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Xunit;
+
 namespace Backend.Test;
 
 public class UnitTest1
@@ -5,9 +7,7 @@ public class UnitTest1
     [Fact]
     public void HelloWorld_ReturnsExpectedMessage()
     {
-        // Simulate logic you'd expect in /helloworld
         string result = "Hello World backend .Net - PR merged!";
-
         Assert.Equal("Hello World backend .Net - PR merged!", result);
     }
 }
